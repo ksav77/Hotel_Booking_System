db3cbc6f6c6a39dbf0a56d65f6be64efa00105cf
chiranjivi7 <81355405+chiranjivi7@users.noreply.github.com>
2021-05-16 10:18:55 +0545

    Delete HotelManagementSystem directory

--- HotelManagementSystem/Readme.txt
+++
@@ -1,1 +0,0 @@
-All codes are under Room directory.
--- HotelManagementSystem/Room/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ImagePost, HomeImage, contact, FoodImage, Bookform
-
-# admin.site.register(Room)
-admin.site.register(ImagePost)
-admin.site.register(HomeImage)
-admin.site.register(contact)
-admin.site.register(FoodImage)
-admin.site.register(Bookform)
